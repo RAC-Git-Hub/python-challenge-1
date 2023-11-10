74217a8e69f025580a57ca856a17e2df7f50439f
RAC-Git-Hub <147117856+RAC-Git-Hub@users.noreply.github.com>
2023-11-09 21:17:08 -0500

    Python file for the python-challenge-1 assignment

--- menu.py
+++ menu.py
@@ -194,7 +194,6 @@ print("This is what we are preparing for you.\n")
 print("Item name                 | Price  | Quantity")
 print("--------------------------|--------|----------")
 
-
 # 6. Loop through the items in the customer's order
 for item in order_list:
         
